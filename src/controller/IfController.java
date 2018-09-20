b433978ed3dc2c2b7a017a03181f1cb0fd844dfe
Jtmonkeyboy <43076688+Jtmonkeyboy@users.noreply.github.com>
2018-09-20 11:46:12 -0600

    I made a commit

---
+++ src/controller/IfController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import model.Guitar.java;
+
+import javax.swing.JOptionPane;
+
+public class IfController
+{
+	public void start()
+	{
+		JOptionPane.showMessageDialog(null, "Hi this is the If project");
+	}
+}
