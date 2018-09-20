49ef2d0928fa4ca6238bc9091d87cab04c197eb7
Jtmonkeyboy <43076688+Jtmonkeyboy@users.noreply.github.com>
2018-09-20 12:22:29 -0600

    I made a piano default.

--- src/model/Piano.java
+++ src/model/Piano.java
@@ -8,9 +8,17 @@ public class Piano
 	private boolean hasStrings;
 	private int keys;
 	
+	/**
+	 * Default constructor for a piano.
+	 * Initializes all variables to valid but "bad" values.
+	 * Used for later customization.
+	 */
 	public Piano()
 	{
-		
+		this.name = "El Piano";
+		this.color = "Black";
+		this.hasStrings = true;
+		this.keys = 88;
 	}
 	
 	public Piano(String name, String color, boolean isMovable, boolean hasStrings, int keys)
